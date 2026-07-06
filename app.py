20f24b265fbe52698b49431120c5f73b1a83ec1e
Pablo Duarte <pa.duartec@duocuc.cl>
2026-07-06 11:57:27 -0400

    Menu principal listo hasta opcion 3

--- app.py
+++ app.py
@@ -15,3 +15,55 @@ Inventario = {
     "P104":[8,25]
 }
 
+while True:
+    os.system("cls")
+    mo.mostrar_menu()
+    opcion = mo.leer_opcion()
+    
+    match opcion:
+        case 1:
+            os.system("cls")
+            categoria = input("Ingrese categoria: ").strip()
+            
+            mo.stock_categoria(productos,Inventario)
+            
+            os.symlinkstem("pause")
+        case 2:
+            os.system("cls")
+            while True:
+                try:
+                    precio_min = int(input("Ingrese precio minimo: "))
+                    precio_max = int(input("Ingrese precio maximo: "))
+                    if precio_min < 0 or precio_max < 0:
+                        print("Error, Debe ingresar un valor mayor a 0")
+                        continue
+                    if precio_min > precio_max:
+                        print("Error, El precio minimo no puede ser mayor al precio maximo")
+                        continue
+                    break
+                except ValueError:
+                    print("Error, Debe ingresar un valor numerico")
+            mo.buscar_precio_rango(productos,precio_min,precio_max)
+            os.system("pause")
+        case 3:
+            os.system("cls")
+            while True:
+                codigo = input("Ingrese codigo del producto: ").strip()
+                if not mo.buscar_codigo(codigo, productos):
+                    while True:
+                        try:
+                            nuevo_precio = int(input("Ingrese nuevo precio: "))
+                            if not mo.validar_precio(nuevo_precio):
+                                print("Error, Debe ingresar un valor mayor a 0")
+                                continue
+                            break
+                        except ValueError:
+                            print("Error, Debe ingresar un valor numerico")
+                    mo.actualizar_precio(codigo, productos, nuevo_precio)
+                    print("Precio actualizado correctamente")
+                else:
+                    print("Error, El codigo ingresado no existe")
+                continue_input = input("Desea actualizar otro producto? (s/n): ").strip().lower()
+                if continue_input != 's':
+                    break
+                os.system("pause")
