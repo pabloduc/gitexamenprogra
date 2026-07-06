c844c92ab524607626e15d5f540d1d80c2930e58
Pablo Duarte <pa.duartec@duocuc.cl>
2026-07-06 11:39:39 -0400

    Leer opcion y buscar codigo listos

--- modulo.py
+++ modulo.py
@@ -12,3 +12,19 @@ def mostrar_menu():
     7. Salir
     ====================================
     """)
+
+def leer_opcion():
+    while True:
+        try:
+            opcion = int(input("Ingrese opcion: "))
+            if opcion >= 1 and opcion <= 7:
+                return opcion
+            print("Error, Debe seleccionar una opcion valida")
+        except ValueError:
+            print("Error, Debe seleccionar una opcion valida")
+
+def buscar_codigo(codigo, productos):
+    for cod in productos:
+        if cod.lower() == codigo.lower():
+            return True
+    return False
